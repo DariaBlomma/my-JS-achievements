aefb9e10f02ec2f7852f3c9c89c61a6f17338bc3
daria <kozina.flowers@yandex.ru>
2021-03-25 22:09:40 +0300

    GloAcademy.Day2.Db, fetch, promise, async etc

--- GloAcademy. Intensive Wildberries/js/main.js
+++ GloAcademy. Intensive Wildberries/js/main.js
@@ -28,13 +28,23 @@ modalClose.addEventListener('click', closeModal);
 modalCart.addEventListener('click', closeModal);
 
 //scroll smooth
-const scrollLinks = document.querySelectorAll('a.scroll-link');
+{const scrollLinks = document.querySelectorAll('a.scroll-link');
 
-(function () {
-	for (let i = 0; i < scrollLinks.length; i++) {
-		scrollLinks[i].addEventListener ('click', function(event) {
+ 
+	// for (let i = 0; i < scrollLinks.length; i++) {
+	// 	scrollLinks[i].addEventListener ('click', function(event) {
+	// 		event.preventDefault();
+	// 		const id = scrollLinks[i].getAttribute('href');
+	// 		document.querySelector(id).scrollIntoView({
+	// 			behavior: 'smooth',
+	// 			block: 'start',
+	// 		});
+	// 	})
+	// }
+	for (const scrollLink of scrollLinks) {
+		scrollLink.addEventListener ('click', function(event) {
 			event.preventDefault();
-			const id = scrollLinks[i].getAttribute('href');
+			const id = scrollLink.getAttribute('href');
 			document.querySelector(id).scrollIntoView({
 				behavior: 'smooth',
 				block: 'start',
@@ -42,4 +52,144 @@ const scrollLinks = document.querySelectorAll('a.scroll-link');
 		})
 	}
 	
-})()
+}
+
+
+//goods
+const viewAll = document.querySelectorAll('.view-all');
+const navigationLink = document.querySelectorAll('.navigation-link:not(.view-all');
+const longGoodsList = document.querySelector('.long-goods-list');
+const AllAccessories = document.querySelectorAll('.all-accessories');
+const AllClothing = document.querySelectorAll('.all-clothing');
+
+
+const getGoods = async function () {
+	const result = await fetch('db/db.json');
+	if (!result.ok) {
+		throw 'Error ' + result.status 
+	}
+	return await result.json();
+};
+
+// getGoods().then(function (data) {
+// 	console.log(data);
+// });
+
+const createCard = function ({ label, name, img, description, id, price }) {
+	const card = document.createElement('div');
+	card.className = 'col-lg-3 col-sm-6';
+	
+	card.innerHTML = `
+		<div class="goods-card">
+		${label ?
+			 `<span class="label">${label}</span>` :
+			  ""}
+			
+			<img src="db/${img}" alt="${name}" class="goods-image">
+			<h3 class="goods-title">${name}</h3>
+			<p class="goods-description">${description}</p>
+			<button class="button goods-card-btn add-to-cart" data-id="${id}">
+				<span class="button-price">$${price}</span>
+			</button>
+		</div>
+	`;
+
+	return card;
+}
+
+const renderCards = function(data) {
+	longGoodsList.textContent = '';
+	const cards = data.map(createCard);
+	// cards.forEach( function(card) {
+	// 	longGoodsList.append(card);
+	// });
+	longGoodsList.append(...cards);
+	document.body.classList.add('show-goods');
+};
+
+const showAll = function(event) {
+	event.preventDefault();
+	getGoods().then(renderCards);
+};
+
+viewAll.forEach(function(elem) {
+	elem.addEventListener('click', showAll);
+});
+// const id = viewAll.getAttribute('href');
+// document.querySelector(id).scrollIntoView({
+// 		behavior: 'smooth',
+// 		block: 'start',
+// 	});
+
+const filterCards = function  (field, value) {
+	getGoods()
+	.then(function(data) {
+		const filteredGoods = data.filter(function(good) {
+			return good[field] === value;
+		});
+		return filteredGoods;
+	})
+	.then(renderCards);
+}
+
+navigationLink.forEach(function (link) {
+	link.addEventListener('click', function (event) {
+		event.preventDefault();
+		
+		
+		const field = link.dataset.field;
+		const value = link.textContent;
+
+		// if (value == 'All') {
+		// 	return getGoods().then(renderCards);
+		// }
+
+		filterCards(field, value);
+		
+	})
+})
+
+
+
+// AllAccessories.addEventListener('click', function (event) {
+// 	event.preventDefault();
+// 	const field = AllAccessories.dataset.field;
+// 	const value = "Accessories";
+// 	filterCards(field, value);
+// 	const id = AllAccessories.getAttribute('href');
+// 	document.querySelector(id).scrollIntoView({
+// 		behavior: 'smooth',
+// 		block: 'start',
+// 	});
+// });
+
+AllAccessories.forEach(item => {
+	item.addEventListener('click', event => {
+		event.preventDefault();	
+		filterCards('category', 'Accessories');	
+		
+			const id = item.getAttribute('href');
+			document.querySelector(id).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start',
+			});
+		
+		
+	});
+});
+
+AllClothing.forEach(item => {
+	item.addEventListener('click', event => {
+		event.preventDefault();
+		filterCards('category', 'Clothing');
+
+		const id = item.getAttribute('href');
+			document.querySelector(id).scrollIntoView({
+				behavior: 'smooth',
+				block: 'start',
+			});
+	});
+});
+
+
+
