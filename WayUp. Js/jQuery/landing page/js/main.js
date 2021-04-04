c5f0d872d871241996bec786e819904d70482b26
Daria Kozina <kozina.flowers@yandex.ru>
2021-04-04 22:30:05 +0300

    swiper, validation is added. Map doesnt work

--- WayUp. Js/jQuery/landing page/js/main.js
+++ WayUp. Js/jQuery/landing page/js/main.js
@@ -22,5 +22,50 @@ $('.menu-close').on('click', function() {
 let scene = document.getElementById('scene');
 let parallaxInstance = new Parallax(scene);
 
+//swiper slider  swiperjs.com
+const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    spaceBetween: 50,
+    slidesPerView: 3,
+    loop: true,
+    stopOnLastSlide: false,
+    autoplay: {
+        delay: 2000
+    }
+});
 
-
+//map yandex
+ymaps.ready(init);
+function init(){
+    let myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 7,
+        controls: ['zoomControl', 'geolocationControl']
+    });
+}
+//form validation
+$.validator.addMethod('regex', function (value, element, regexp) {
+    let regExp = new RegExp(regexp);
+    return regExp.test(value)
+}, 'Please check your input');
+    
+$('form').validate({ 
+    rules: {
+        firstName: { 
+            required: true,
+            regex: "[A-Za-z]{1-32}"
+        },
+        phoneNumber: { 
+            required: true,
+            digits: true,
+            minlength: 10,
+            maxlength: 11,
+            regex: "[0-9]+"
+        }
+    },
+    messages: {
+        firstName: 'Write the name correctly',
+        phoneNumber: 'Write your phone number correctly'
+    }
+});
